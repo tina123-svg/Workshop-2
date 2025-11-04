4cff276aa3b79d7571b4ef2e1d29ba87167ef4c7
tina123-svg <tinasitaula835@gmail.com>
2025-11-04 16:07:21 +0545

    task 6

---
+++ task6/Program.cs
@@ -0,0 +1,19 @@
+﻿List<string> fruits = new List<string> { "Orange", "Apple", "Banana" };
+
+fruits.Add("Mango");
+fruits.Remove("Orange");
+
+Console.WriteLine("Fruits List: ");
+foreach (string fruit in fruits)
+{
+    Console.WriteLine(fruit);
+}
+
+Dictionary<int, string> fruitDict = new Dictionary<int, string> { { 1, "Apple" }, { 2, "Banana" }, { 3, "Mango" } };
+fruitDict.Add(4, "Grapes");
+
+Console.WriteLine("Fruit Dictionary: ");
+foreach (KeyValuePair<int, string> item in fruitDict)
+{
+    Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+}
