182458a5b08b8c966c78f595c9972392ae07459c
tina123-svg <tinasitaula835@gmail.com>
2025-11-04 15:23:26 +0545

    task 3

---
+++ task3/Program.cs
@@ -0,0 +1,23 @@
+﻿byte a = 1;
+short b = 2;
+int c = 3;
+float d = 4.4f;
+double e = 3.55;
+char f = 'A';
+bool g = true;
+
+int num = 42;
+string numStr = num.ToString();
+
+string piStr = "3.14";
+double piDouble = double.Parse(piStr);
+
+Console.WriteLine($"Byte: {a}");
+Console.WriteLine($"Short: {b}");
+Console.WriteLine($"Int: {c}");
+Console.WriteLine($"Float: {d}");
+Console.WriteLine($"Double: {e}");
+Console.WriteLine($"Char: {f}");
+Console.WriteLine($"Bool: {g}");
+Console.WriteLine($"Converted int to str: {numStr}");
+Console.WriteLine($"Converted str to double: {piDouble}");
