2e0b9503bcfa57c2d4b0f740599f2c738b5a7a11
tina123-svg <tinasitaula835@gmail.com>
2025-11-04 15:16:02 +0545

    task 2

---
+++ task2/Program.cs
@@ -0,0 +1,9 @@
+﻿class Circle
+{
+    const double PI = 3.14;
+
+    static void Main (string [] args)
+    {
+        PI = 2.123; // This line will return a compilation error because the value of a constant cannot be changed.
+    }
+}
