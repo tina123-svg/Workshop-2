59fbf99fd4fea40c5491e0f25c6906b1bf1b0d04
tina123-svg <tinasitaula835@gmail.com>
2025-11-04 15:58:34 +0545

    task 5

---
+++ task5/Program.cs
@@ -0,0 +1,13 @@
+﻿DateTime birthDate = new DateTime(2005, 6, 15);
+
+DateTime currentDate = DateTime.Now;
+
+TimeSpan age = currentDate - birthDate;
+int ageYear = (int)(age.TotalDays / 362.25);
+
+Console.WriteLine($"Birth Date: {birthDate.ToShortDateString()}");
+Console.WriteLine($"Current Date: {currentDate.ToShortDateString()}");
+Console.WriteLine($"Age in years: {ageYear}");
+
+DateTime newBirthDate = birthDate.AddDays(10);
+Console.WriteLine($"New BirthDate is: {newBirthDate.ToShortDateString()}");
